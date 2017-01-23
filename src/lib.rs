f5e47dec47eb8efe60491fd003ba9d30319654a2
Steve Melia <steve.j.melia@gmail.com>
2017-01-23 07:21:29 +0000

    Move the generic to the impl

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,17 @@ use std::fs;
 use std::fs::File;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::marker::PhantomData;
 use std::path::Path;
 use uuid::Uuid;
 
-pub struct Db {
+pub struct Db<T> {
     file: File,
+    _phantom: PhantomData<T>,
 }
 
-impl Db {
-    pub fn create<P: AsRef<Path>>(path: P) -> Result<Db, DbError> {
+impl<T: Encodable + Decodable> Db<T> {
+    pub fn create<P: AsRef<Path>>(path: P) -> Result<Db<T>, DbError> {
         let mut file = fs::OpenOptions::new()
             .read(true)
             .write(true)
@@ -36,13 +38,14 @@ impl Db {
         }
 
         let db = Db {
-            file: file
+            file: file,
+            _phantom: PhantomData,
         };
 
         Ok(db)
     }
 
-    pub fn put<T: Encodable>(&mut self, key: Uuid, value: T) -> Result<(), DbError> {
+    pub fn put(&mut self, key: Uuid, value: T) -> Result<(), DbError> {
         self.file.seek(SeekFrom::End(0))?;
         encode_into(&key, &mut self.file, SizeLimit::Infinite)?;
         encode_into(&value, &mut self.file, SizeLimit::Infinite)?;
@@ -53,7 +56,7 @@ impl Db {
         Ok(())
     }
 
-    pub fn get<T: Decodable + std::fmt::Display>(&mut self, key: Uuid) -> Result<Option<T>, DbError> {
+    pub fn get(&mut self, key: Uuid) -> Result<Option<T>, DbError> {
         self.file.seek(SeekFrom::Start(0))?;
         let record_count : u64 = decode_from(&mut self.file, SizeLimit::Infinite)?;
 
@@ -83,8 +86,8 @@ mod tests {
         let mut db = Db::create("put_then_get_returns_expected").unwrap();
         let key = Uuid::new_v4();
         let value = "this is a test transmission";
-        db.put(key, value).unwrap();
-        let retrieved = db.get::<String>(key).unwrap().expect("No value retrieved");
+        db.put(key, String::from(value)).unwrap();
+        let retrieved = db.get(key).unwrap().expect("No value retrieved");
         remove_file("put_then_get_returns_expected").unwrap();
         assert_eq!(retrieved, value);
     }
@@ -97,9 +100,9 @@ mod tests {
         let mut db = Db::create("put_put_then_get_returns_expected").unwrap();
         let key = Uuid::new_v4();
         let value = "this is a test transmission";
-        db.put(Uuid::new_v4(), "valueA").unwrap();
-        db.put(key, value).unwrap();
-        let retrieved = db.get::<String>(key).unwrap().expect("No value retrieved");
+        db.put(Uuid::new_v4(), String::from("valueA")).unwrap();
+        db.put(key, String::from(value)).unwrap();
+        let retrieved = db.get(key).unwrap().expect("No value retrieved");
         remove_file("put_put_then_get_returns_expected").unwrap();
         assert_eq!(retrieved, value);
     }
@@ -112,9 +115,9 @@ mod tests {
         let mut db = Db::create("get_returns_not_found").unwrap();
         let key = Uuid::new_v4();
         let value = "this is a test transmission";
-        db.put(Uuid::new_v4(), "valueA").unwrap();
-        db.put(key, value).unwrap();
-        let retrieved = db.get::<String>(Uuid::new_v4()).unwrap();
+        db.put(Uuid::new_v4(), String::from("valueA")).unwrap();
+        db.put(key, String::from(value)).unwrap();
+        let retrieved = db.get(Uuid::new_v4()).unwrap();
         remove_file("get_returns_not_found").unwrap();
         assert_eq!(retrieved.is_some(), false);
     }
